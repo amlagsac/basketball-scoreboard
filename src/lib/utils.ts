e6645f5d5a54ed9437e6f7aee43ff9a2da5d73e6
amlagsac <andremlagsac@gmail.com>
2025-02-27 15:46:50 +0800

    Fix buzzer sounds not working

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,16 @@
+import { assets } from '$app/paths';
+
 export function playLongBuzzerSound(): void {
-	const longBuzzerSound: HTMLAudioElement = new Audio('/sounds/LongBuzzerSound.mp3');
+	const longBuzzerSound: HTMLAudioElement = new Audio(`${assets}/sounds/LongBuzzerSound.mp3`);
 	longBuzzerSound.play();
 }
 
 export function playShotClockSound(): void {
-	const shotClockSound: HTMLAudioElement = new Audio('/sounds/ShotClock.mp3');
+	const shotClockSound: HTMLAudioElement = new Audio(`${assets}/sounds/ShotClock.mp3`);
 	shotClockSound.play();
 }
 
 export function playSubSound(): void {
-	const subSound: HTMLAudioElement = new Audio('/sounds/SubBuzzer.mp3');
+	const subSound: HTMLAudioElement = new Audio(`${assets}/sounds/SubBuzzer.mp3`);
 	subSound.play();
 }
