b897c6a101faa42d651405c01cafe454cfcabe61
amlagsac <andremlagsac@gmail.com>
2025-03-01 23:35:50 +0800

    Fix left out sm breakpoint

--- src/routes/components/OtherDetailsBox.svelte
+++ src/routes/components/OtherDetailsBox.svelte
@@ -2,12 +2,12 @@
 	let { boxLabel, boxValue, isFoul = false } = $props();
 </script>
 
-<div class="flex flex-col items-center gap-2 sm:gap-1">
-	<h3 class="text-md text-center font-[Impact] tracking-wider text-white sm:text-2xl">
+<div class="flex flex-col items-center gap-2 md:gap-1">
+	<h3 class="text-md text-center font-[Impact] tracking-wider text-white md:text-2xl">
 		{boxLabel}
 	</h3>
 	<div class="rounded-sm border border-white bg-black px-2 py-1 text-center md:px-5 md:py-1">
-		<span class="font-[Digital-7] text-5xl leading-none text-red-500 sm:text-[8rem]"
+		<span class="font-[Digital-7] text-5xl leading-none text-red-500 md:text-[8rem]"
 			>{isFoul && boxValue > 4 ? 'P' : boxValue}</span
 		>
 	</div>
